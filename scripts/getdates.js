93d7d61e1761ac16c598c1c90b3dfddc4f654424
legend-ctl <mjohn3@byupathway.edu>
2025-09-08 15:00:54 +0100

    initial commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,12 @@
+
 const today = new Date();
 const year = today.getFullYear();
 document.querySelector("#currentyear").textContent = `©${year} 🌲 Moses John Godwin 🌲 Lagos, Nigeria.`;
 const lastModified = document.lastModified;
 document.querySelector("#lastModified").textContent =`Last Modified: ${lastModified}`;
+
+
+const courseTitle = document.querySelector("#course-title")
+if (courseTitle){
+    courseTitle.textContent += " | Moses John"
+}
