8a830cbcdb190f1bed853dac0f926319ac109c8a
legend-ctl <mjohn3@byupathway.edu>
2025-09-12 20:07:36 +0100

    initial commit

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -3,3 +3,12 @@ const year = today.getFullYear();
 document.querySelector("#currentyear").textContent = `©${year} 🌲 Moses John Godwin 🌲 Lagos, Nigeria.`;
 const lastModified = document.lastModified;
 document.querySelector("#lastModified").textContent = `Last Modified: ${lastModified}`;
+
+
+const mainnav = docucument.querySelector("ul")
+const hambutton = document.querySelector("#menu")
+
+hambutton.addEventListener("click", () => {
+    mainnav.classList.toggle("show")
+    hambutton.classList.toggle("show")
+})
