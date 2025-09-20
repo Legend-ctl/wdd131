5b697bef1179e484bd81a1e49770d21821ddb358
legend-ctl <mjohn3@byupathway.edu>
2025-09-20 15:52:32 +0100

    initial commit

--- practice3/scripts/index.js
+++ practice3/scripts/index.js
@@ -1,9 +1,20 @@
-const btn = document.querySelector("button")
+// const btn = document.querySelector("button")
 
-function random(number) {
-    return Math.floor(Math.random() * (number + 1))
+// function random(number) {
+//     return Math.floor(Math.random() * (number + 1))
+// }
+// btn.addEventListener("click", () => {
+//     const rndColor = `rgb(${random(255)} ${random(255)} ${random(255)})`
+//     document.body.style.backgroundColor = rndColor
+// })
+
+
+let scores = [100, 72, 83, 94, 88, 87];
+let names = ['Nancy','Blessing','Jorge','Svetlana'];
+
+for (let i = 0; i < scores.length; i++) {
+  console.log(scores[i]);
 }
-btn.addEventListener("click", () => {
-    const rndColor = `rgb(${random(255)} ${random(255)} ${random(255)})`
-    document.body.style.backgroundColor = rndColor
-})
+
+
+
---
+++ scripts/place.js
@@ -0,0 +1,7 @@
+let date = new Date();
+let year = date.getFullYear();
+document.querySelector("#date").textContent = `©${year} 🌲 Moses John Godwin 🌲 Nigeria`;
+
+
+const lastModified = document.lastModified;
+document.querySelector("#lastModified").textContent = `Last Modified: ${lastModified}`
