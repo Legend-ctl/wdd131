f6b46eb95b78a93322ee49532c5d2bafd3aba0fa
legend-ctl <mjohn3@byupathway.edu>
2025-09-25 11:34:54 +0100

    initial commit

--- scripts/place.js
+++ scripts/place.js
@@ -5,3 +5,20 @@ document.querySelector("#date").textContent = `©${year} 🌲 Moses John Godwin 
 
 const lastModified = document.lastModified;
 document.querySelector("#lastModified").textContent = `Last Modified: ${lastModified}`
+
+
+let temperature = 28;  
+let windSpeed = 10;    
+
+function calculateWindChill(temp, speed) {
+  return 13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16);
+}
+
+let windChill;
+if (temperature <= 10 && windSpeed > 4.8) {
+  windChill = calculateWindChill(temperature, windSpeed).toFixed(1) + " °C";
+} else {
+  windChill = "N/A";
+}
+
+document.querySelector("#windChill").textContent = `Wind Chill: ${windChill}`;
